30dd43d99c88280f7d6899ba65a01b9d77bfb24f
NageshMane <mane46740@gmail.com>
2024-09-08 18:19:18 +0530

    Cloudinary Section

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,7 +1,7 @@
 import {v2 as cloudinary} from "cloudinary";
 import dotenv from "dotenv";
 dotenv.config();
-
+//Cloud Name , API key , API Secret
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY,
